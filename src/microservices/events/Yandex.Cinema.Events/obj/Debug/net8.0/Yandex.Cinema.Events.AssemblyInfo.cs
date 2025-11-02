ce395794cd15c8082dd9ef892a05869c4046945b
alukyanov <alukyanov@gemsdev.com>
2025-11-02 22:20:12 +0600

    =

--- src/microservices/events/Yandex.Cinema.Events/obj/Debug/net8.0/Yandex.Cinema.Events.AssemblyInfo.cs
+++ src/microservices/events/Yandex.Cinema.Events/obj/Debug/net8.0/Yandex.Cinema.Events.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yandex.Cinema.Events")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f41c66ac8119f46748718908582b1a978dc2a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8403a90ea5ccdb2fa997ebd68a0907e1493d16aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yandex.Cinema.Events")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yandex.Cinema.Events")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
